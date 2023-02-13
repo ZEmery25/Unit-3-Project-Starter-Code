49d3e82b87b86c6d28ca54efe7c58827bb2012cb
ZEmery25 <125311332+ZEmery25@users.noreply.github.com>
2023-02-13 14:35:32 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,37 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+let confrontButton = document.querySelector(".option-one");
+let buildButton = document.querySelector(".option-two");
+//two different first option screen
+let confrontDiv = document.querySelector(".option-one-screen");
+let buildDiv = document.querySelector(".option-two-screen");
+//second button options
+//two different end option screens
+let kidnappDiv = document.querySelector(".option-one-end");
+let savedDiv = document.querySelector(".option-two-end");
+//body
+let body = document.querySelector("body");
+
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
